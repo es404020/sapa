08ddf74014cdc5a6c80cf03ab72f4175875731f7
es404020 <es404020@gmail.com>
2021-12-22 18:49:37 +0100

    ok

---
+++ start.sh
@@ -0,0 +1,31 @@
+# yarn add @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^4
+
+# mkdir themes && cd themes && touch themes.js
+
+
+# cd themes
+
+# echo   'import { extendTheme } from "@chakra-ui/react"
+# const colors = {brand: {
+#       900: "#1a365d",
+#       800: "#153e75",
+#       700: "#01048a",
+#     },
+#   }
+
+
+# const config = {
+#   initialColorMode: "light",
+#   useSystemColorMode: false,
+# }
+
+# const theme = extendTheme({ config,colors ,fonts: {
+#   heading: "Poppins",
+#   body: "Poppins",
+# },})
+
+# export default theme
+
+# ' >> themes.js
+
+
