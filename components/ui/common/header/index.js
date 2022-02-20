cbec0ed408f332d65fb2c2220dc9c9a888894110
es404020 <es404020@gmail.com>
2022-02-20 04:56:49 -0800

    connection with metamask

--- components/ui/common/header/index.js
+++ components/ui/common/header/index.js
@@ -34,13 +34,52 @@ import {
     Center,
   } from "@chakra-ui/react";
 
-
+  import { useWeb3 } from "@components/provider";
+  import { useAccount } from "@components/hooks/web3";
 
 export default function index({toggle}) {
+
+  const { connect, isWeb3Loaded, isLoading } = useWeb3();
+
+
+  const { account } = useAccount();
     return (
        <Flex p={3} width="100%" alignItems="center" height="10%" justifyContent="space-between" boxShadow="md" >
             <Text>Block</Text>
+            <Flex alignItems="center"  >
+
+            {isLoading ? (
+                <Button  mr={10}>loading .....</Button>
+              ) : isWeb3Loaded ? account.data? (
+                <Button   mr={10} className="cursor-default">Hi there {account.isAdmin && "Admin"}</Button>
+              ): (
+                <Button onClick={connect}
+
+                borderRadius="20px"
+                size="sm"
+                color={{ base: "black", md: "white" }}
+                bg={{ base: "white", md: " #7928CA" }}
+                mr={10}
+                
+                >Connect</Button>
+              ) 
+              
+              
+              : (
+                <Button
+                mr={10}
+                  onClick={() => {
+                    window.open("https://metamask.io/download", "_blank");
+                  }}
+                >
+                  Install Metamask
+                </Button>
+              )}
+              
+         
             {toggle}
+            </Flex>
+          
        </Flex>
     )
 }
--- pages/main/index.js
+++ pages/main/index.js
@@ -1,78 +1,141 @@
-import React from 'react';
+import React from "react";
 
 import { BaseLayout } from "@components/ui/layout";
+import { PhoneIcon, AddIcon, WarningIcon } from "@chakra-ui/icons";
 import {
-    Flex,
-    Text,
-    Image,
-    Spacer,
-    useColorModeValue,
-    WrapItem,
-    InputRightElement,
-    Heading,
-    Progress,
-    Box,
-    Button,
-    List,
-    ListItem,
-    ListIcon,
-    HStack,
-    Icon,
-    InputGroup,
-    InputLeftElement,
-    Input,
-    Grid,
-    Select,
-    Table,
-    Circle,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    Avatar,
-    Square,
-    position,
-    Center,
+  Flex,
+  Text,
+  Tab,
+  TabList,
+  Tabs,
+  TabPanel,
+  TabPanels,
+  Image,
+  Spacer,
+  useColorModeValue,
+  WrapItem,
+  InputRightElement,
+  Heading,
+  Progress,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListIcon,
+  HStack,
+  Icon,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Grid,
+  Select,
+  Table,
+  Circle,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  Avatar,
+  Square,
+  position,
+  Center,
 } from "@chakra-ui/react";
+import { useAccount,useNetwork } from "@components/hooks/web3";
 
 export default function Home() {
-    return (
-        <Flex flexDirection="column" p={4} >
-            <Flex flexDirection="row" height="80%" justifyContent="space-between">
-            <Flex
-            boxShadow="dark-lg"
-                flexDirection="row"
-                borderRadius="md"
-                my={2}
-                alignItems="center"
-                justifyContent="space-between"
-                p={3}
-               
-                width="30%"
+
+  const { account } = useAccount();
+  const {network} = useNetwork();
+  return (
+    <Flex flexDirection="column" p={4}>
+      <Flex flexDirection="row" height="80%" justifyContent="space-between">
+        <Flex
+          boxShadow="dark-lg"
+          flexDirection="row"
+          borderRadius="md"
+          my={2}
+          alignItems="center"
+          justifyContent="space-between"
+          p={5}
+          width="30%"
+        >
+          <Flex
+            mr={5}
+            alignItems="center"
+            width="100%"
+            justifyContent="space-between"
+            flexDirection="row"
+          >
+            <Avatar size="sm" name="0X4568" src="" />
+            <Flex flexDirection="column" alignItems="center">
+              <Text fontSize="sm">{account.data}</Text>
               
-              
-            >
-                <Avatar size="sm" name="0X4568" src="" />
-                <Flex flexDirection="column" alignItems="center">
-                    <Text fontSize="sm">0X4568...067f</Text>
-                    <Text fontSize="sm" fontWeight="bold">
-                        Ethe-Mainet
-                    </Text>
-                </Flex>
-            </Flex>
 
-            <Flex flexDirection="column">
-                <Text fontSize="sm" fontWeight="md">Total Amount</Text>
-                <Text fontWeight="extrabold" fontSize="xx-large">$6,000,000</Text>
-
+                {!network.isSupported && network.isLoading && (
+           <Text fontSize="sm" fontWeight="bold">
+           {network.target}
+           </Text>
+          )}
+            
             </Flex>
-            </Flex>
-           
+          </Flex>
 
+          <Flex
+            width="100%"
+            alignItems="center"
+            justifyContent="space-between"
+            flexDirection="row"
+          >
+            <PhoneIcon />
+            <PhoneIcon />
+            <PhoneIcon />
+          </Flex>
         </Flex>
-    )
+
+        <Flex flexDirection="column" width="30%">
+          <Text fontSize="sm" fontWeight="md">
+            Total Amount
+          </Text>
+
+          <Flex
+            width="100%"
+            alignItems="center"
+            justifyContent="space-between"
+            flexDirection="row"
+          >
+            <Text fontWeight="extrabold" fontSize="xx-large">
+              $6,000,000
+            </Text>
+            <PhoneIcon />
+            <PhoneIcon />
+            <PhoneIcon />
+          </Flex>
+        </Flex>
+      </Flex>
+
+      <Tabs mt={5}>
+        <TabList>
+          <Tab>Portfolio</Tab>
+          <Tab>NFT</Tab>
+          <Tab>History</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+  );
 }
 
-
 Home.Layout = BaseLayout;
+//illness wash goose pair soap usual plunge fat idea napkin remind material
